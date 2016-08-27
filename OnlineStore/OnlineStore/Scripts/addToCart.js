69d661bf7dfd1c8db550a254c0060cfc90d7e8da
Slupov <stoyan.lupov@gmail.com>
2016-08-27 15:54:07 +0300

    Carts added
    FIX : Add to cart action

---
+++ OnlineStore/OnlineStore/Scripts/addToCart.js
@@ -0,0 +1,13 @@
+﻿$("#myLink").click(function(e){
+
+    e.preventDefault();
+    $.ajax({
+
+       
+    success: function(){
+        alert("Value Added");  // or any other indication if you want to show
+    }
+
+});
+
+});
